24218efd2b42b2e76861962fe40792295204d3c4
Quinn <145577147+quintavious101@users.noreply.github.com>
2026-07-27 13:51:54 +0000

    finished 01 04

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,12 @@
+// Imports
+import scala.util.Using
+import scala.io.Source
+import java.io.File
+import java.io.FileWriter
+
 @main def run: Unit =
-  val input: String = ???
-  val output: String = ???
+  val input: String = ""
+  val output: String = ""
   etl(input, output)
 
 def etl(inputFilePath: String, outputFilePath: String): Unit =
@@ -9,9 +15,17 @@ def etl(inputFilePath: String, outputFilePath: String): Unit =
   load(transformed, outputFilePath)
 end etl
 
-def extract(input: String): List[String] = ???
-def transform(data: List[String]): List[String] = ???
+def extract(input: String): List[String] = Using.resource(Source.fromFile(input))(_.getLines.toList)
+
+// map() is a higher order function because it takes other functions as parameters and/or returns functions as a result
+def transform(data: List[String]): List[String] = data.map(_.toLowerCase)
+
 def load(
     data: List[String],
     output: String = "src/main/resources/output.txt"
-): Unit = ???
+): Unit = 
+  val file = new File(output)
+  val fileWriter = new FileWriter(file)
+  fileWriter.write(data.mkString("\n"))
+  fileWriter.close()
+end load
