0b9afccb304f9bb8d438276b7408e50dee4f4eeb
Quinn <145577147+quintavious101@users.noreply.github.com>
2026-07-29 19:42:22 +0000

    finished 0202

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,4 +9,6 @@ import FileUtils.*
   val output: String = "src/main/resources/output.txt"
 
   import Etl.IntImpl
-  etl(input, output)
+  etl(input, output) match
+    case Left(error) => println("Failure: error")
+    case Right(value) => println("Success :)")
